de0e0fd558a0af3f1e6a2ec57f2f4b3317c156a6
jrr45 <jrr45@harpoon.(none)>
2012-07-14 01:11:37 -0400

    Fix some bugs with timesteps and other issues

--- ace3p_animation.py
+++ ace3p_animation.py
@@ -4,27 +4,42 @@ from paraview.simple import *
 class Ace3pAnimataion(object):
     """holds all information related to the animation of ACE3P"""
     def __init__(self, mesh_file,
-             mode_file,
+             mode_files=[],
              trac3p_results="",
-             reflections=[] # http://www.itk.org/Wiki/ParaView/Users_Guide/List_of_filters#Reflect
+             reflections=[], # http://www.itk.org/Wiki/ParaView/Users_Guide/List_of_filters#Reflect
+             sidesetIDs=[6],
+             field='efield',
+             VectorMode='Magnitude',#['Magnitude', 'Component']
+             VectorComponent='X',#['X','Y', 'Z'] only used if VectorMode='Component'
+             ColorSpace='HSV',
+             RGBmin=[0.0, 0.0, 1.0],
+             RGBmax=[1.0, 0.0, 0.0],
+             Representation='Wireframe', #['Outline', 'Points', 'Wireframe', 'Surface', 'Surface With Edges'] 
+             BackfaceRepresentation='Surface'
              ):
          
         # prevent goofy camera behavior
         paraview.simple._DisableFirstRenderCameraReset()
         
         # check the existance of the given files
+        mode_files.sort()
         self.use_track3p = trac3p_results != ""
         self.mesh_file = mesh_file
-        self.mode_file = mode_file
+        self.mode_files = mode_files
         self.trac3p_results = trac3p_results
         
         if not os.path.exists(mesh_file) or not os.path.isfile(mesh_file):
             print "mesh file was not found: %s" % mesh_file
             sys.exit(0)
         
-        if not os.path.exists(mode_file) or not os.path.isfile(mode_file):
-            print "mode file was not found: %s" % mode_file
+        if len(mode_files) == 0:
+            print "there must be at least one mode file"
             sys.exit(0)
+                
+        for mode_file in mode_files:
+            if not os.path.exists(mode_file) or not os.path.isfile(mode_file):
+                print "mode file was not found: %s" % mode_file
+                sys.exit(0)
             
         if self.use_track3p:
             particledir = os.path.join(trac3p_results, "PARTICLES")
@@ -44,81 +59,66 @@ class Ace3pAnimataion(object):
 
         # Load mesh
         self.mesh = SLACDataReader(MeshFileName=self.mesh_file)
-        self.mesh.ModeFileName = [self.mode_file]
+        self.mesh.ModeFileName = self.mode_files
         self.mesh.ReadInternalVolume = 1
+        self.mesh.UpdatePipelineInformation()
         
         if self.use_track3p:
             # Load the particles
             self.p_ts = SLACParticleDataReader(FileName=self.particleFilenames)
-            self.times = self.p_ts.TimestepValues
-        else:
-            self.times = self.mesh.TimestepValues.GetData()
-
-        a3_efield_PVLookupTable = GetLookupTableForArray("efield", 3,
-                                                          RGBPoints=[0.0, 0.0, 0.0, 1.0, 22.701986109850427, 1.0, 0.0, 0.0],
-                                                          VectorMode='Magnitude',
-                                                          ColorSpace='HSV',
-                                                          ScalarRangeInitialized=1.0)
-        a3_efield_PiecewiseFunction = CreatePiecewiseFunction()
-        
-        SetActiveSource(self.mesh)
-        DataRepresentation1 = Show()
-        DataRepresentation1.EdgeColor = [0.0, 0.0, 0.0]
-        DataRepresentation1.ScalarOpacityUnitDistance = 0.034588492113178465
-        DataRepresentation1.ScalarOpacityFunction = a3_efield_PiecewiseFunction
-        DataRepresentation1.ColorArrayName = 'efield'
-        DataRepresentation1.LookupTable = a3_efield_PVLookupTable
-        DataRepresentation1.ColorAttributeType = 'POINT_DATA'
-        DataRepresentation1.Representation = 'Wireframe'
-        DataRepresentation1.BackfaceRepresentation = 'Surface'
-        DataRepresentation1.Visibility = 0
-        
-        #outline box
-        DataRepresentation2 = Show()
-        DataRepresentation2.Visibility = 0
-        DataRepresentation2.Representation = 'Outline'
-        DataRepresentation2.EdgeColor = [0.0, 0.0, 0.0]
-        
-        #AnimationScene1 = GetAnimationScene()
-        #AnimationScene1.EndTime = 6.2421777983841767e-07
-        #AnimationScene1.PlayMode = 'Snap To TimeSteps'
-        if self.use_track3p:
-            SetActiveSource(self.p_ts)
-            DataRepresentation3 = Show()
-            DataRepresentation3.EdgeColor = [0.0, 0.0, 0.0]
-            DataRepresentation3.Visibility = 0
             
-        a3_efield_PVLookupTable.RGBPoints = [0.0, 0.0, 0.0, 1.0, 22.701986109850427, 1.0, 0.0, 0.0]
+        field_data = self.mesh.PointData.GetArray(field)
+        if field_data is None:
+            print "invalid field name: %s" % field
+            sys.exit(0)
         
+        # set of the coloring of the mesh
+        field_range = field_data.GetRange()
+        RGBPoints=[max(0, field_range[0]), # first point is the min of the range
+                   RGBmin[0], RGBmin[1], RGBmin[2], #the color at the minimum
+                   max(abs(field_range[0]), abs(field_range[1])), #max of the field range
+                   RGBmax[0], RGBmax[1], RGBmax[2]] #the color at the maximum
+
+        if VectorMode == 'Component':
+            comp = {'X': 0, 'Y': 1, 'Z': 2, 'x': 0, 'y': 1, 'z': 2}
+            field_PVLookupTable = GetLookupTableForArray(field, 3,
+                                                         RGBPoints=RGBPoints,
+                                                         VectorMode='Component',
+                                                         VectorComponent=comp[VectorComponent],
+                                                         ColorSpace=ColorSpace,
+                                                         ScalarRangeInitialized=1.0)
+        else:
+            field_PVLookupTable = GetLookupTableForArray(field, 3,
+                                                         RGBPoints=RGBPoints,
+                                                         VectorMode=VectorMode,
+                                                         ColorSpace=ColorSpace,
+                                                         ScalarRangeInitialized=1.0)
+        field_PiecewiseFunction = CreatePiecewiseFunction()
+        
+        # select the mesh surfaces that match the given Cubit sidesets
         SetActiveSource(self.mesh)
         ExtractBlock1 = ExtractBlock()
-        ExtractBlock1.BlockIndices = [7]
+        ExtractBlock1.BlockIndices = [x+1 for x in sidesetIDs]
         
-        DataRepresentation4 = Show()
-        DataRepresentation4.EdgeColor = [0.0, 0.0, 0.0]
-        DataRepresentation4.ColorAttributeType = 'POINT_DATA'
-        DataRepresentation4.ScalarOpacityFunction = a3_efield_PiecewiseFunction
-        DataRepresentation4.ColorArrayName = 'efield'
-        DataRepresentation4.ScalarOpacityUnitDistance = 0.037695299910287722
-        DataRepresentation4.Texture = []
-        DataRepresentation4.LookupTable = a3_efield_PVLookupTable
-        DataRepresentation4.Visibility = 0
-        
-        # reflect mesh
+        # reflect mesh oven the given planes
         for plane in self.reflections:
             Reflect1 = Reflect()
             Reflect1.Plane = plane
+            
+        # magic number???
+        ScalarOpacityUnitDistance = 1.0
         
+        # Draw the fields
         DataRepresentation5 = Show()
         DataRepresentation5.EdgeColor = [0.0, 0.0, 0.0]
         DataRepresentation5.ColorAttributeType = 'POINT_DATA'
-        DataRepresentation5.ScalarOpacityFunction = a3_efield_PiecewiseFunction
-        DataRepresentation5.ColorArrayName = 'efield'
-        DataRepresentation5.ScalarOpacityUnitDistance = 0.030463782178688604
+        DataRepresentation5.ScalarOpacityFunction = field_PiecewiseFunction
+        DataRepresentation5.ColorArrayName = field
+        DataRepresentation5.ScalarOpacityUnitDistance = ScalarOpacityUnitDistance
         DataRepresentation5.Texture = []
-        DataRepresentation5.LookupTable = a3_efield_PVLookupTable
-        DataRepresentation5.Representation = 'Wireframe'
-        DataRepresentation5.BackfaceRepresentation = 'Surface'
+        DataRepresentation5.LookupTable = field_PVLookupTable
+        DataRepresentation5.Representation = Representation
+        DataRepresentation5.BackfaceRepresentation = BackfaceRepresentation
         
         #reflect particles
         if self.use_track3p:
@@ -129,11 +129,24 @@ class Ace3pAnimataion(object):
                 Reflect1.Plane = plane
             
             DataRepresentation7 = Show()
-            DataRepresentation7.ScalarOpacityUnitDistance = 0.043229431222641161
+            DataRepresentation7.ScalarOpacityUnitDistance = ScalarOpacityUnitDistance
             DataRepresentation7.Texture = []
             DataRepresentation7.EdgeColor = [0.0, 0.0, 0.0]
     
         UpdatePipeline()
+        
+        #get time information
+        if self.use_track3p:
+            self.times = self.p_ts.TimestepValues
+        else:
+            SetActiveSource(self.mesh)
+            self.times = self.mesh.TimestepValues
+            # Some mode files will not have TimeSteps so generate some
+            if len(self.times) == 0:
+                timerange = self.mesh.GetProperty('TimeRange') 
+                steps = 100
+                self.times = [timerange[1]*i/steps + timerange[0] for i in range(steps)]
+                
         
     def get_bounds(self):
         bounds = self.mesh.GetDataInformation().GetBounds() 
@@ -171,9 +184,13 @@ class Ace3pAnimataion(object):
         y = (ybounds[1]+ybounds[0])/2
         z = (zbounds[1]+zbounds[0])/2
         return x, y, z
+    
+    def get_frame_range(self):
+        return [0, len(self.times)]
         
     def play(self, camera, background_color=[0.0, 0.0, 0.0],
-             antialias=1, peels=4, GUI=True, mono=True, stereo=False, speedup = 1):
+             antialias=1, peels=4, GUI=True, mono=True, stereo=False, 
+             speedup = 1, frame_range=None):
         view = GetRenderView()
         view.Background = background_color # background color
         # Controls the number of passes in the rendering algorithm. A higher 
@@ -182,59 +199,72 @@ class Ace3pAnimataion(object):
         view.MaximumNumberOfPeels = peels
         viewScale = 4
         view.ViewSize = [480, 270]
+        view.UseLight = 1
         
         if GUI:
             camera.update_camera(0)
             Render()
+            return
+        
+        times = self.times
+        lentimes = len(times)
+        digits = len(str(lentimes))
+        
+        if self.use_track3p:
+            outFilenameEnd = "_particles.png"
         else:
-            times = self.times
-            lentimes = len(times)
-            digits = len(str(lentimes))
+            outFilenameEnd = "_mod.png"
             
-            if self.use_track3p:
-                outFilenameEnd = "_particles.png"
+        if not os.path.exists("movie_images"):
+            os.mkdir("movie_images")
+        
+        # set the range to be used in the loop
+        if frame_range is not None:
+            if frame_range[0] >= 0 and frame_range[1] <= lentimes and \
+               frame_range[0] < frame_range[1]:
+                timerange = range(frame_range[0], frame_range[1])
             else:
-                outFilenameEnd = "_mod.png"
-                
-            if not os.path.exists("movie_images"):
-                os.mkdir("movie_images")
-            
-            for ts in range(lentimes):
+                raise ValueError("invalid frame range")
+        else:
+            timerange = range(lentimes)
+        
+        for ts in timerange:
 
-                if ts % speedup != 0:
-                    continue
-                
-                camera.update_camera(ts)
-                
-                now = times[ts] # simulation time
-                outFilename = "movie_images/" + str(ts).zfill(digits) + outFilenameEnd
-                
-                print "===> Computing for '%s' ... of %i" % (outFilename, lentimes-1)
-                
-                ##########################################################################
-                # update time to now, and show the results 
-                view.ViewTime = now
-        
-                if stereo:
-                    view.StereoRender = 1
-        
-                    view.StereoType = 'Left'
-                    Show()
-                    WriteImage("L_%s" % outFilename, Magnification=viewScale*antialias)
-        
-                    view.StereoType = 'Right'
-                    Show()
-                    WriteImage("R_%s" % outFilename, Magnification=viewScale*antialias)
-        
-                if mono:
-                    view.StereoRender = 0
-                    Show()
-                    WriteImage(outFilename, Magnification=viewScale*antialias)
-                    
+            if ts % speedup != 0:
+                continue
+            
+            camera.update_camera(ts)
+            
+            now = times[ts] # simulation time
+            outFilename = "movie_images/" + str(ts).zfill(digits) + outFilenameEnd
+            
+            print "===> Computing for '%s' ... of %i" % (outFilename, lentimes-1)
+            
+            ##########################################################################
+            # update time to now, and show the results 
+            view.ViewTime = now
+    
             if stereo:
-                print "boo"
-            else:
-                print "now run:"
-                print ("ffmpeg -i movie_images/%0" + str(digits) + "d_particles.png -s 1920x1080 " +
-                      "movie.mp4")
-
+                view.StereoRender = 1
+    
+                view.StereoType = 'Left'
+                Show()
+                WriteImage("L_%s" % outFilename, Magnification=viewScale*antialias)
+    
+                view.StereoType = 'Right'
+                Show()
+                WriteImage("R_%s" % outFilename, Magnification=viewScale*antialias)
+    
+            if mono:
+                view.StereoRender = 0
+                Show()
+                WriteImage(outFilename, Magnification=viewScale*antialias)
+                
+        if stereo:
+            print ("The ffmpeg command will vary depending on how you are constructing the " +
+                  "3D movie")
+            print "the command for a 2D movie would be:"
+        else:
+            print "now run:"
+        print ("ffmpeg -i movie_images/%0" + str(digits) + outFilenameEnd + " -s 1920x1080 " +
+               "movie.mp4")
--- generic_pvframes.py
+++ generic_pvframes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-##!/usr/bin/pvbatch --use-offscreen-rendering
 
 ################################################################################
 
@@ -8,21 +7,24 @@ except: from paraview.simple import *
 from ace3p_animation import Ace3pAnimataion
 from general_camera import GeneralCamera, CameraMovement
 from math import pi
+import glob
 
 ################################################################################
-anime = Ace3pAnimataion(mesh_file = '/home/jrr45/small_gap/small_gap_6.0.ncdf',
-            mode_file = '/home/jrr45/small_gap/omega3p_results_6.0/omega3p.l0.m0000.1.4957339e+09.mod',
-            trac3p_results = "/home/jrr45/small_gap/track3p_results_6.0/",
-            reflections=['X'])
+root = "/home/jrr45/ptesting/taper/"
+anime = Ace3pAnimataion(mesh_file = '/home/jrr45/gun_current/gun.ncdf',#mesh_file = (root + "taper.ncdf"),
+            mode_files = ['/home/jrr45/gun_current/omega3p_results/omega3p.l0.m0000.1.4950919e+09.mod'], #mode_files = glob.glob(root+"/t3p_results/OUTPUT/mymonts_t[0-9]*ps.out.mod"),#trac3p_results = "/home/jrr45/small_gap/track3p_results_6.0/",
+            reflections=['X'],
+            sidesetIDs=[4,6],
+            field='efield')
 
 ################################################################################
 #find the center of the mesh
 x, y, z = anime.get_center()
 
 camera = GeneralCamera(camera_movements=[],
-                          focus=[x, y, z], 
-                          radius=.83, theta=-pi/2, phi=43*pi/100,
-                          up=[-1.0,0.0,0.0], psi=0)
+                       focus=[x, y, z], 
+                       radius=.83, theta=-pi/2, phi=43*pi/100,
+                       up=[-1.0,0.0,0.0], psi=0)
 
-anime.play(camera=camera, 
+anime.play(camera=camera, background_color=[0.0, 0.0, 0.0],
            GUI=False, antialias=2)
